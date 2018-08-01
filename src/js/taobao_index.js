c6596c48c061dbd01600154df75842443a9c66a5
xuyongpeng <1771510445@qq.com>
2018-08-01 21:02:32 +0800

    第二次

---
+++ src/js/taobao_index.js
@@ -0,0 +1,16 @@
+new Swiper(".banner", {
+    loop: true,
+    autoplay: {
+        delay: 2000
+    },
+    pagination: {
+        el: ".swiper-pagination"
+    }
+})
+new Swiper(".con", {
+    loop: true,
+    autoplay: {
+        delay: 2000
+    },
+    direction: "vertical"
+})
